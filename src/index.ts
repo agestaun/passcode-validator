aa564dd575cf240671d8477b0398834ed9f97c28
Adrián García <a.g.estaun@gmail.com>
2022-12-04 13:12:02 +0100

    Exporting new SpecialCharRule

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import LengthRule from './rules/LengthRule';
 import LowercaseRule from './rules/LowercaseRule';
 import PINRule from './rules/PINRule';
+import SpecialCharRule from './rules/SpecialCharRule';
 import UppercaseRule from './rules/UppercaseRule';
 import Validator from './Validator';
 
-export { UppercaseRule, LowercaseRule, LengthRule, PINRule };
+export { UppercaseRule, LowercaseRule, LengthRule, PINRule, SpecialCharRule };
 export default Validator;
